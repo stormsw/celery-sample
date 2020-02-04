32e9a703cf32dab8613c690111f5f172d4ce9d91
Alexander Varchenko <alexander.varchenko@gmail.com>
2020-02-04 17:21:15 +0200

    Added sample when lock on error

--- workers/test_celery/run_tasks.py
+++ workers/test_celery/run_tasks.py
@@ -1,14 +1,33 @@
 from __future__ import absolute_import
-from .tasks import longtime_add
+from .tasks import longtime_add, rise, test_a,test_b,test_c
+
+from celery import chain, chord
 import time
 
 if __name__ == '__main__':
-    result = longtime_add.delay(1,2)
-    # at this time, our task is not finished, so it will return False
-    print ('Task finished? ', result.ready())
-    print ('Task result: ', result.result)
-    # sleep 10 seconds to ensure the task has been finished
-    time.sleep(10)
-    # now the task should be finished and ready method will return True
-    print ('Task finished? ', result.ready())
-    print ('Task result: ', result.result)
+    # result = longtime_add.delay(1,2)
+    # # at this time, our task is not finished, so it will return False
+    # print ('Task finished? ', result.ready())
+    # print ('Task result: ', result.result)
+    # # sleep 10 seconds to ensure the task has been finished
+    # time.sleep(10)
+    # # now the task should be finished and ready method will return True
+    # print ('Task finished? ', result.ready())
+    # print ('Task result: ', result.result)
+    #chord([ a1.s(1)  , a1.s(2) , a1.s(3)  ])(b1.s()).get()
+    chord([ ( test_a.s(1) | test_b.s()) , ( test_c.s(2) | test_b.s()), ( test_a.s(3) | test_b.s())  ])(test_c.s()).get()
+    #test(add=True)
+    # res = chain(rise.s(10), longtime_add.s(2,8)).apply_async()
+    # res.get()
+
+def test(main=False, add=False, sumall=False, mul=False):
+    taskid = some_test.delay(
+        main,
+        add,
+        sumall,
+        mul
+    ).get()
+
+    res = MfAsyncResult(taskid).get()
+
+    print('Res is', res, 'type is: ', type(res))
--- workers/test_celery/tasks.py
+++ workers/test_celery/tasks.py
@@ -1,7 +1,51 @@
 from __future__ import absolute_import
 from .app import app
+from celery import Task
+from celery.result import AsyncResult 
 import time
 
+class CeleryException(Exception):
+    task_id = None
+    task_name = None
+
+    def __init__(self, task_id, task_name, exc):
+        self.task_id = task_id
+        self.task_name = task_name
+        self.exc = exc
+
+    def __str__(self):
+        return 'Exception raised in celery taskId: ' + \
+            self.task_id + '. TaskName: ' + self.task_name + '.' \
+            'Exception: ' + str(self.exc)
+
+
+class MfTask(Task):
+    def __call__(self, *args, **kwargs):
+        # If an error has been raised from a previous task, pass down the error
+        for arg in args:
+            # Need to handle list of chord
+            if type(arg) is list:
+                for el in arg:
+                    if type(el) is CeleryException:
+                        return el
+
+            if type(arg) is CeleryException:
+                return arg
+
+        try:
+            return Task.__call__(self, *args, **kwargs)
+        except Exception as e:
+            return CeleryException(self.request.id, self.name, e)
+
+
+class MfAsyncResult(AsyncResult):
+    def get(self, *args):
+        res = AsyncResult.get(self, args)
+
+        if type(res) is CeleryException:
+            raise res
+
+        return res
 
 @app.task(name='tasks.longtime_add')
 def longtime_add(x, y):
@@ -10,3 +54,70 @@ def longtime_add(x, y):
     time.sleep(5)
     print('long time task finished')
     return x + y
+
+@app.task(name='tasks.rise')
+def rise(x):
+    time.sleep(x)
+    raise Exception("error")
+
+@app.task(base=MfTask)
+def add(x, y, failes):
+    if failes:
+        raise Exception('Failing in add')
+    return x + y
+
+@app.task
+def test_a(arg):
+    print('Task A1, starring with arg:' + str(arg))
+    if arg == 3:
+        raise Exception('A1 Error')
+    time.sleep(5)
+    print('A1 done')
+    return {'a1': True}
+
+@app.task
+def test_b(arg):
+    print('Task A2, starring with arg:' + str(arg))
+    time.sleep(5)
+    print('A2 done')
+    return {'a2': True}
+
+@app.task
+def test_c(arg):
+    print('Task B1, starring with arg:' + str(arg))
+    time.sleep(5)
+    print('B1 done')
+    return {'b1': True}
+
+@app.task(base=MfTask)
+def mul(x, y, failes):
+    if failes:
+        raise Exception('Failing in mul')
+    return x * y
+
+
+@app.task(base=MfTask)
+def sumall(v, failes):
+    if failes:
+        raise Exception('Failing in sumall')
+    return sum(v)
+
+
+@app.task(base=MfTask)
+def some_test(main_f, add_f, sumall_f, mul_f):
+    nbs = [1, 2, 3, 4]
+
+    add_chord = chord(
+        (add.s(nb, 1, add_f) for nb in nbs),
+        sumall.s(sumall_f)
+    )
+
+    add_async = chain(
+        add_chord, mul.s(2, mul_f)).apply_async()
+
+    if main_f:
+        raise Exception('Failing in some_test')
+
+    return add_async.task_id
+
+
